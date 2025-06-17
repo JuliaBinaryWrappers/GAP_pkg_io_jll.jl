d981d6bff15ad620d03094860f5aeb5f6c02dcc1
jlbuild <juliabuildbot@gmail.com>
2025-06-17 15:49:48 +0000

    GAP_pkg_io_jll build 400.900.101+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -2,11 +2,10 @@
 export io
 
 using GAP_jll
-using GAP_lib_jll
 JLLWrappers.@generate_wrapper_header("GAP_pkg_io")
 JLLWrappers.@declare_file_product(io)
 function __init__()
-    JLLWrappers.@generate_init_header(GAP_jll, GAP_lib_jll)
+    JLLWrappers.@generate_init_header(GAP_jll)
     JLLWrappers.@init_file_product(
         io,
         "lib/gap/io.so",
