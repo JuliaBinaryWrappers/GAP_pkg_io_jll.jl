7f0cdebc9e26347ce948a27be1ca4cf4ae4f310b
jlbuild <juliabuildbot@gmail.com>
2026-06-02 18:37:15 +0000

    GAP_pkg_io_jll build 401.90300.1+0

--- src/GAP_pkg_io_jll.jl
+++ src/GAP_pkg_io_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("GAP_pkg_io")
-JLLWrappers.@generate_main_file("GAP_pkg_io", UUID("19a9678b-966c-568e-9f2e-41ceac70c829"))
+JLLWrappers.@generate_main_file("GAP_pkg_io", Base.UUID("19a9678b-966c-568e-9f2e-41ceac70c829"))
 end  # module GAP_pkg_io_jll
